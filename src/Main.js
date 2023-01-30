cf76028a80f0eab1e9c7a0af0159af11e04e3a9b
Will Burket <willburket119@gmail.com>
2023-01-29 21:19:34 -0500

    items displayed after search

--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,31 @@
 import React, {createContext, useContext, useEffect, useState} from "react";
 import { MusicKitContext } from "./MusicKitContext";
 
-
 const SearchContext = createContext(null);  
 
 function Main (){
     const music = useContext(MusicKitContext);
     const [searchResult, setSearchResult] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        console.log(searchResult)
-        // console.log(typeof searchResult)
+        console.log(searchResult)       // getting called twice for some reason    
     },[searchResult]);
 
     function SearchButton (){
         
-        async function searchMusic(){
-            const { data: result } = await music.api.music('v1/me/library/albums');
-    
-            setSearchResult({searchResult: result.data});
-            // User's iCloud Music Library Albums
-            //console.log(result.data);
+        async function searchMusic(){    
+            setLoading(true)
+            try{
+                const { data: result } = await music.api.music('v1/me/library/albums'); // users albums 
+                setSearchResult([...result.data])
+            }
+            catch(err){
+                console.log(err)
+            }
+            finally{
+                setLoading(false)
+            }
         }
     
         return(
@@ -31,27 +36,28 @@ function Main (){
             </li>
         );
     }
-    
+        if(loading) return <p>Loading...</p>
 
-    return(
-        <div>
-            <SearchButton/>
-            <SearchContext.Provider value = {searchResult}>
-                
-            {/* {searchResult.keys(key.id).map(key => (
-            <div key={key.id}>
-            <p>{id}: {searchResult[key.id].attributes.artistName}</p>
-            <p>{id}: {searchResult[key.id].attributes.name}</p>
+        return(
+            <div>
+                <SearchButton/>
+                <SearchContext.Provider value = {searchResult}>
+                <div>
+                    {searchResult ? (
+                    <ul>
+                    {searchResult.map(item => (
+                    <li key={item.id}>{item.attributes.artistName}: {item.attributes.name}</li>
+                    ))}
+                    </ul>
+                    ) : (
+                    <p>No items found</p>
+                    )}
+                </div>        
+                </SearchContext.Provider>
             </div>
-            ))}
-                 */}
-            </SearchContext.Provider>
-        </div>
-    )
+        );
 }
 
 export default Main;
 
-// app
-        // navbar
-            // search
+
