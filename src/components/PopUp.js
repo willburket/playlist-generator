99ff5c2abad7c2b46aebdbc9877a51bd64eb98c1
Will Burket <willburket119@gmail.com>
2023-02-27 22:57:22 -0500

    added query param to script tag to prevent caching

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -1,11 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import { MusicKitContext } from "../App";
+import { ReactComponent as Account} from '../assets/images/account.svg'
 
 
 function PopUp(){
     const music = useContext(MusicKitContext); 
     const [showPopup, setShowPopup] = useState(false);
     const [poppedUp, setPoppedUp] = useState(false);
+    
 
     const handleCloseDiv = () => {
         setShowPopup(false)
@@ -56,6 +58,9 @@ function PopUp(){
             <div className="popup-container">
                 {showPopup && 
                 <div className="popup">
+                    <div className="account-container">
+                        <Account/>
+                    </div>
                     <p>Log into Apple Music to listen to the full song</p>
                     <PopUpButton text = "Ok"/>
                 </div>}
