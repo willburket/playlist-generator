77360a78fba8f773890b65d4517f9b95a88b7907
Will Burket <willburket119@gmail.com>
2023-03-16 20:55:23 -0400

    file structure change

--- server/TokenConfig.js
+++ server/TokenConfig.js
@@ -4,8 +4,8 @@ require('dotenv').config()
 
 
 const private_key = fs.readFileSync(process.env.API_KEY).toString(); // read your private key from your file system
-const team_id = 'S3PHBMG8Q4'; // your 10 character apple team id, found in https://developer.apple.com/account/#/membership/
-const key_id = 'PA6274NFYR'; // your 10 character generated music key id. more info https://help.apple.com/developer-account/#/dev646934554
+const team_id = process.env.TEAM_ID; // your 10 character apple team id, found in https://developer.apple.com/account/#/membership/
+const key_id = process.env.KEY_ID; // your 10 character generated music key id. more info https://help.apple.com/developer-account/#/dev646934554
 const token = jwt.sign({}, private_key, {
   algorithm: 'ES256',
   expiresIn: '180d',
