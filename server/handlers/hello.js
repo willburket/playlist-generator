f6c8f969d6cf0b0293ea9798afb103591b4c0cc9
Will Burket <willburket119@gmail.com>
2023-05-21 11:45:06 -0400

    serverless migration of token

---
+++ server/handlers/hello.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'server configured successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports = {
+  handler: hello
+}
--- server/handlers/token.js
+++ server/handlers/token.js
@@ -2,20 +2,37 @@ const fs = require('fs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
+const tokenSign = async () => {
+  const private_key = fs.readFileSync(process.env.API_KEY).toString(); // read your private key from your file system
+  const team_id = process.env.TEAM_ID; // your 10 character apple team id, found in https://developer.apple.com/account/#/membership/
+  const key_id = process.env.KEY_ID; // your 10 character generated music key id. more info https://help.apple.com/developer-account/#/dev646934554
+  const token = jwt.sign({}, private_key, {
+    algorithm: 'ES256',
+    expiresIn: '180d',
+    issuer: team_id,
+    header: {
+      alg: 'ES256',
+      kid: key_id
+    }
+  });
 
-const private_key = fs.readFileSync(process.env.API_KEY).toString(); // read your private key from your file system
-const team_id = process.env.TEAM_ID; // your 10 character apple team id, found in https://developer.apple.com/account/#/membership/
-const key_id = process.env.KEY_ID; // your 10 character generated music key id. more info https://help.apple.com/developer-account/#/dev646934554
-const token = jwt.sign({}, private_key, {
-  algorithm: 'ES256',
-  expiresIn: '180d',
-  issuer: team_id,
-  header: {
-    alg: 'ES256',
-    kid: key_id
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: token, 
+      },
+      null,
+      2
+    ),
   }
-});
+
+  return response;
+
+} 
+
+
 
 module.exports = {
-  token               //handler: token ??
+  handler: tokenSign               //handler: token ??
 }
